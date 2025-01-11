5fe489f23f6150032127e2f057577809c3dc2e80
THANH-HUY <thanhhuy1999.uet@gmail.com>
2025-01-11 16:35:05 +0700

    implement logging for system

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 import db from "./models";
 import bodyParser from "body-parser";
 import { seedData } from "./seed/seed-data";
@@ -9,6 +12,30 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+// setup for logging
+// create path
+const logDir = path.join(__dirname, 'logs');
+const accessLogFile = path.join(logDir, 'access.log');
+
+// automatic create folder if dont exist
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+    console.log(`Created log directory: ${logDir}`);
+}
+
+// automatic create logging file if dont exist
+if (!fs.existsSync(accessLogFile)) {
+    fs.writeFileSync(accessLogFile, '', { flag: 'w' });
+    console.log(`Created log file: ${accessLogFile}`);
+}
+
+// create stream logging
+const accessLogStream = fs.createWriteStream(accessLogFile, { flags: 'a' }); //overwirte data
+
+// using morgan
+app.use(morgan('combined', { stream: accessLogStream })); //detail infor in file
+app.use(morgan('dev')); // simple infor in console
 
 app.get("/", (req: any, res: any) => {
     res.send("Hello world!");
